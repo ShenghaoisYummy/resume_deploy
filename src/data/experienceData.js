20323755f8caa21af0c1bf169d09b97f22874215
ShenghaoisYummy <hsupisces@hotmail.com>
2024-01-26 18:40:22 +1100

    added data folder to store group info

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,40 +1,40 @@
 const experiences = [
-    {
-      date: "Jul 20 - Jan 2022",
-      compSrc: "/src/assets/images/experience/apple.svg",
-      loc: "Cupertino",
-      jobTitle: "Product Designer",
-      company: "Apple",
-      description:
-        "Omnis minima inventore minus. Aut et incidunt. Aut fugiat culpa illum optio dolorum aut maxime ipsa. Laborum incidunt enim consectetur perspiciatis. Dolore ullam dolor impedit dolorum recusandae facilis quo et. Et ipsam vel sunt qui ut officia voluptatem.",
-    },
-    {
-      date: "Jul 20 - Jan 2022",
-      compSrc: "/src/assets/images/experience/apple.svg",
-      loc: "Cupertino",
-      jobTitle: "Product Designer",
-      company: "Apple",
-      description:
-        "Omnis minima inventore minus. Aut et incidunt. Aut fugiat culpa illum optio dolorum aut maxime ipsa. Laborum incidunt enim consectetur perspiciatis. Dolore ullam dolor impedit dolorum recusandae facilis quo et. Et ipsam vel sunt qui ut officia voluptatem.",
-    },
-    {
-      date: "Jul 20 - Jan 2022",
-      compSrc: "/src/assets/images/experience/apple.svg",
-      loc: "Cupertino",
-      jobTitle: "Product Designer",
-      company: "Apple",
-      description:
-        "Omnis minima inventore minus. Aut et incidunt. Aut fugiat culpa illum optio dolorum aut maxime ipsa. Laborum incidunt enim consectetur perspiciatis. Dolore ullam dolor impedit dolorum recusandae facilis quo et. Et ipsam vel sunt qui ut officia voluptatem.",
-    },
-    {
-      date: "Jul 20 - Jan 2022",
-      compSrc: "/src/assets/images/experience/apple.svg",
-      loc: "Cupertino",
-      jobTitle: "Product Designer",
-      company: "Apple",
-      description:
-        "Omnis minima inventore minus. Aut et incidunt. Aut fugiat culpa illum optio dolorum aut maxime ipsa. Laborum incidunt enim consectetur perspiciatis. Dolore ullam dolor impedit dolorum recusandae facilis quo et. Et ipsam vel sunt qui ut officia voluptatem.",
-    },
-  ];
+  {
+    date: "Jul 20 - Jan 2022",
+    compSrc: "/src/assets/images/experience/apple.svg",
+    loc: "Cupertino",
+    jobTitle: "Product Designer",
+    company: "Apple",
+    description:
+      "Omnis minima inventore minus. Aut et incidunt. Aut fugiat culpa illum optio dolorum aut maxime ipsa. Laborum incidunt enim consectetur perspiciatis. Dolore ullam dolor impedit dolorum recusandae facilis quo et. Et ipsam vel sunt qui ut officia voluptatem.",
+  },
+  {
+    date: "Oct 2015 - Mar 2020",
+    compSrc: "/src/assets/images/experience/tesla.svg",
+    loc: "Austin",
+    jobTitle: "UX Designer",
+    company: "Tesla",
+    description:
+      "Commodi atque sit ut qui assumenda earum aperiam occaecati voluptates. Fuga quas aut soluta nostrum et beatae. Maiores repudiandae quibusdam autem enim ut in possimus ad.",
+  },
+  {
+    date: "Sep 2014 - Aug 2015",
+    compSrc: "/src/assets/images/experience/google.svg",
+    loc: "Mountain View",
+    jobTitle: "Design system architect",
+    company: "Google",
+    description:
+      "Ut molestias animi. Neque voluptate velit corporis adipisci voluptate et qui sed neque. Inventore eos non. Qui eveniet quo incidunt nemo but I have no idea why life always so hard.",
+  },
+  {
+    date: "Sep 2010 - Jul 2013",
+    compSrc: "/src/assets/images/experience/vector.svg",
+    loc: "Berlin",
+    jobTitle: "Design system architect",
+    company: "Vectornator",
+    description:
+      "Non in fugiat aut consequatur deleniti. Consequuntur impedit modi asperiores impedit ut ea aut eligendi doloremque. Fugit laborum ducimus hic tempore velit. Sed vitae et corrupti omnis temporibus. Unde voluptas quaerat ea rerum aspernatur autem assumenda animi fugit. Omnis laborum velit voluptatum similique accusantium eveniet voluptatem.",
+  },
+];
 
-  export default experiences;
+export default experiences;
